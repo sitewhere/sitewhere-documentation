22a807d98fcfe4fe405a868354d43f0d9e58d4ec
Derek Adams <derek.adams@sitewhere.com>
2017-06-05 13:08:17 -0400

    Doing initial testing of Sphinx Gradle plugin.

---
+++ src/site/sphinx/conf.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import sys, os
+from recommonmark.parser import CommonMarkParser
+
+project = u'SiteWhere'
+copyright = u'2017, SiteWhere LLC'
+version = '1.11'
+release = '1.11.0'
+
+# General options
+needs_sphinx = '1.0'
+master_doc = 'index'
+pygments_style = 'tango'
+add_function_parentheses = True
+
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.plantuml', 'javasphinx']
+templates_path = ['_templates']
+exclude_trees = ['.build']
+source_suffix = ['.rst', '.md']
+source_encoding = 'utf-8-sig'
+source_parsers = {
+  '.md': CommonMarkParser
+}
+
+# HTML options
+html_theme = 'sphinx_rtd_theme'
+html_short_title = "sitewhere"
+htmlhelp_basename = 'sitewhere-doc'
+html_use_index = True
+html_use_smartypants = True
+html_show_sourcelink = False
+html_static_path = ['_static']
+
+# PlantUML options
+plantuml = os.getenv('plantuml')
---
+++ src/site/sphinx/index.rst
@@ -0,0 +1,8 @@
+SiteWhere Documentation
+=======================
+
+This is an example of SiteWhere documentation.
+
+First Header
+------------
+This is the first header.
